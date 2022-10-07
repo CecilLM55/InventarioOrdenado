63eda9365f07779acf17c3428176396d8853640b
CecilLM55 <clopez16@ucol.mx>
2022-10-07 16:54:37 -0500

    primer avance de agregar

--- inventario.js
+++ inventario.js
@@ -4,11 +4,29 @@ class Inventario {
         this.inventario = new Array();
     }
 
-    agregar(producto) {
+    agregar(producto, codigo) {
 
+       /* this.inventario.push(producto);
+       */
+
+       if((this.inventario === 0)) {
         this.inventario.push(producto);
+       } else {
+
+        for(let i = 0; i < this.inventario.length; i++) {
+
+            if(this.inventario[i].getCodigo() > codigo) {
+                
+                for(let b = this.inventario.length; b > i; b--) {
+                    this.inventario[b + i] = this.inventario[b];
+                }
+                this.inventario[i] = producto;
+            }
+        }
+       }
 
     }
+    
 
     eliminar(codigo) {
         let primero = 0;
@@ -33,22 +51,7 @@ class Inventario {
         }
         
         return null;
- /*       
-        for(let i = 0; i < this.inventario.length; i++) {
-            if(codigo === this.inventario[i].getCodigo()) {
-
-                for(let a = i; a < this.inventario.length; a++) {
-                    this.inventario[a] = this.inventario[a + 1];
-                }
-                this.inventario.pop();
-            }
-        }
-*/
-/*
-for(let a = i; a < this.inventario.length; a++) {
-                    this.inventario[a] = this.inventario[a + 1];
-                }
-*/
+ 
     }
 
     buscar(codigo) {
@@ -100,29 +103,3 @@ for(let a = i; a < this.inventario.length; a++) {
 /*    modificar(codigo) {
         
     } */
-
-    /*
-Crear una interfaz en HTML para manipular un INVENTARIO de productos, 
-para almacenar la información debemos usar un vector (no usar BDs ni local storage).
-La información de cada producto debe permitir guardar el código, el 
-nombre,  cantidad y el costo.
-
-LOS ELEMENTOS DEBERÁN AGREGARSE Y QUEDAR ALMACENADOS ORDENADOS DE 
-MANERA ASCENDENTE POR EL CÓDIGO QUE SERÁ NUMÉRICO.
-En la interfaz (una sola pantalla) tener los inputs para cada dato, 
-los botones para las tareas a realizar que se describen a continuación 
-y un div para ir describiendo las actividades que se van realizando 
-y sus resultados.
-Botones :
-Agregar nuevo producto
-Eliminar un producto por código, deberá devolver el producto (si 
-    existe) o null
-Buscar un producto por código
-Recuperar todos los productos //listar
-Recuperar todos los productos en orden inverso a como se ingresaron//listar inverso
-
-No se implementa el INSERTAR ni el MODIFICAR
-
-No usar swal.fire para las interfaces
-Evitar el uso de alert y confirm
-    */
